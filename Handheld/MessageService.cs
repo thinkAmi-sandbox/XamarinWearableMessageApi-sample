f2f643afb3fc8aae7a2d9797d86521e1c5072e80
thinkAmi <dev.thinkami+git@gmail.com>
2015-03-05 06:20:41 +0900

    Add send message function to Handheld project

--- Handheld/MessageService.cs
+++ Handheld/MessageService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 
+using System.Collections.Generic;
+using System.Text;
+using Android.Runtime;  // `JavaCast`を使うために必要
 using Android.Widget; // toastまわり
+using Android.Gms.Common.Apis;
 using Android.Gms.Wearable; 
 
 namespace Handheld
@@ -8,8 +12,12 @@ namespace Handheld
     [Android.App.Service()]
     [Android.App.IntentFilter(new string[] { "com.google.android.gms.wearable.BIND_LISTENER" })] 
     public class MessageService : 
-        Android.Gms.Wearable.WearableListenerService
+        Android.Gms.Wearable.WearableListenerService,
+        IResultCallback,
+        IGoogleApiClientConnectionCallbacks,
+        IGoogleApiClientOnConnectionFailedListener
     {
+        IGoogleApiClient client;
         private const string MessageTag = "hoge";
 
         // WearableListenerServiceのをオーバーライド
@@ -24,6 +32,64 @@ namespace Handheld
                 var msg = System.Text.Encoding.UTF8.GetString(e.GetData());
 
                 Android.Widget.Toast.MakeText(this, msg, ToastLength.Long).Show();
+
+
+                // UIスレッドではないため、以下の書き方でOK
+                var nodeIds = NodeIds;
+                foreach (var nodeId in nodeIds)
+                {
+                    WearableClass.MessageApi
+                        .SendMessage(client, nodeId, MessageTag, Encoding.UTF8.GetBytes("こんにちは"));
+                } 
+            }
+        }
+
+        // IGoogleApiClientConnectionCallbacksインタフェース向け
+        public void OnConnected(Android.OS.Bundle connectionHint)
+        {
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+        }
+
+        // IGoogleApiClientOnConnectionFailedListenerインタフェース向け
+        public void OnConnectionFailed(Android.Gms.Common.ConnectionResult result)
+        {
+        }
+
+        // IResultCallbackインタフェース向け
+        public void OnResult(Java.Lang.Object result)
+        {
+        } 
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            client = new Android.Gms.Common.Apis.GoogleApiClientBuilder(this)
+                .AddApi(Android.Gms.Wearable.WearableClass.Api)
+                .AddConnectionCallbacks(this)
+                .AddOnConnectionFailedListener(this)
+                .Build();
+
+            client.Connect();
+        }
+
+        public ICollection<string> NodeIds
+        {
+            get
+            {
+                var results = new HashSet<string>();
+                var nodes =
+                    WearableClass.NodeApi.GetConnectedNodes(client)
+                        .Await().JavaCast<INodeApiGetConnectedNodesResult>();
+
+                foreach (var node in nodes.Nodes)
+                {
+                    results.Add(node.Id);
+                }
+                return results;
             }
         } 
     }
