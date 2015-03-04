da9223d2905c00defc0709faef1dd4bc23a9318f
thinkAmi <dev.thinkami+git@gmail.com>
2015-03-05 05:43:54 +0900

    Add receive message function to Handheld project

---
+++ Handheld/MessageService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.Widget; // toastまわり
+using Android.Gms.Wearable; 
+
+namespace Handheld
+{
+    [Android.App.Service()]
+    [Android.App.IntentFilter(new string[] { "com.google.android.gms.wearable.BIND_LISTENER" })] 
+    public class MessageService : 
+        Android.Gms.Wearable.WearableListenerService
+    {
+        private const string MessageTag = "hoge";
+
+        // WearableListenerServiceのをオーバーライド
+        public override void OnMessageReceived(IMessageEvent e)
+        {
+            base.OnMessageReceived(e);
+
+            // メッセージタグが一致している場合のみ反応する
+            // See: https://github.com/xamarin/monodroid-samples/blob/master/wear%2FQuiz%2FWearable%2FQuizListenerService.cs#L134
+            if (MessageTag.Equals(e.Path))
+            {
+                var msg = System.Text.Encoding.UTF8.GetString(e.GetData());
+
+                Android.Widget.Toast.MakeText(this, msg, ToastLength.Long).Show();
+            }
+        } 
+    }
+}
+
